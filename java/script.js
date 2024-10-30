74b62ba5cf09c401d6940700e09775270614ee13
Johnatan Oliveira Ferreira <johnatanoliveira353@gmail.com>
2024-10-29 21:21:17 -0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,11 @@
+var DigiteSeuNome = prompt("digite seu nome")
+var DigiteSuaIdade = prompt("digite sua idade")
+var DigiteSeuCurso = prompt("Digite Seu Curso")
+var DigiteSuaProfissao = prompt("Digite Sua Profissão")
+var resultado = DigiteSeuNome + DigiteSuaIdade + DigiteSeuCurso + DigiteSuaProfissao
+alert(`
+    Seu nome é: ${DigiteSeuNome}
+    Sua Idade é: ${DigiteSuaIdade}
+    Seu Curso é: ${DigiteSeuCurso}
+    Sua Profissão é: ${DigiteSuaProfissao}
+    `) 
